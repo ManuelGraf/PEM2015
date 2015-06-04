5c7dd4293cec4f3743ce64a31843c96c0ee46cb0
ManuelGraf <ManuelGraf@github.com>
2015-06-04 13:40:24 +0200

    Step counter + faily bpm calc

--- app/YARA/app/src/main/java/pem/yara/StartActivity.java
+++ app/YARA/app/src/main/java/pem/yara/StartActivity.java
@@ -1,19 +1,90 @@
 package pem.yara;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends ActionBarActivity implements SensorEventListener  {
+
+    private TextView txtStepCount;
+    private SensorManager mSensorManager;
+    private Sensor mStepCounterSensor;
+    private Sensor mStepDetectorSensor;
+
+    private boolean startedStepCounter;
+    private int stepCountInit;
+    private int[] lastSteps;
+
+    public void addStep(int timestamp){
+        for (int i=0; i< lastSteps.length; i++){
+            if(lastSteps[i] != 0){
+                lastSteps[i] = lastSteps[i+1];
+            }
+        }
+        lastSteps[lastSteps.length-1] = timestamp;
+    }
+    public int getBPM(){
+        // calculate average bpm from the last few steps.
+        int startTime = lastSteps[0];
+        int lastTime = lastSteps[lastSteps.length-1];
+        int delta = lastTime - startTime / lastSteps.length-1;
+        int bpm = (delta/1000000) / 60;
+        if(startTime != 0 && lastTime != 0){
+            return bpm;
+        }else{
+            return 0;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        txtStepCount = (TextView)findViewById(R.id.step_count);
+
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        lastSteps = new int[] {0,0,0,0,0};
     }
 
+
+    protected void onResume() {
+
+        super.onResume();
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor s, int i){
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +106,25 @@ public class StartActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+
+        if (values.length > 0) {
+            value = (int) values[0];
+            if(!startedStepCounter){
+                startedStepCounter = true;
+                stepCountInit = value;
+            }
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            txtStepCount.setText("Step Counter Detected : " + (value - stepCountInit)+ ",BPM:"+getBPM());
+            Log.d("StartActivity", lastSteps.toString());
+        }
     }
 }
