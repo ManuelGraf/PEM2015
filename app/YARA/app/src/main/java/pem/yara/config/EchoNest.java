5e833aa0f6b726400a782251e26984b92ef52f54
Martin Reiss <martin.reiss@campus.lmu.de>
2015-06-06 16:24:21 +0200

    Echonest config

---
+++ app/YARA/app/src/main/java/pem/yara/config/EchoNest.java
@@ -0,0 +1,16 @@
+package pem.yara.config;
+
+public class EchoNest {
+
+    public static final String API_KEY = "YUNP8GLWKN942WKOF";
+
+    public static final String CONSUMER_KEY = "39c9bbe82d4098391799ef8eccc930ee";
+
+    public static final String SHARED_SECRET = "xca0NPJwR6yCK9xaXyYleA";
+
+    public static final String USER = "yara";
+
+    public static final String EMAIL = "grafm@cip.ifi.lmu.de";
+
+    public static final String PASSWORD = "49766794";
+}
