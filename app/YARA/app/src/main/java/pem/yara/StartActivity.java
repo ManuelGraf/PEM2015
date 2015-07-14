915733df5a2d9095ebe6384a7f56c44727ff56e1
ManuelGraf <ManuelGraf@github.com>
2015-07-14 14:29:23 +0200

    dont reset dbs

--- app/YARA/app/src/main/java/pem/yara/StartActivity.java
+++ app/YARA/app/src/main/java/pem/yara/StartActivity.java
@@ -11,8 +11,6 @@ import android.widget.TextView;
 import com.astuetz.PagerSlidingTabStrip;
 
 import pem.yara.adapters.HomeScreenPageAdapter;
-import pem.yara.db.RunDbHelper;
-import pem.yara.db.TrackDbHelper;
 import pem.yara.entity.YaraSong;
 import pem.yara.fragments.SongListFragment;
 import pem.yara.music.ScanMusicTask;
@@ -35,8 +33,8 @@ public class StartActivity extends ActionBarActivity implements SongListFragment
 
         txtLoadingIndicator = (TextView)findViewById(R.id.infoBox);
         // TODO: Remove reset-Statements when going live
-        new RunDbHelper(getBaseContext()).resetDB();
-        new TrackDbHelper(getBaseContext()).resetDB();
+//        new RunDbHelper(getBaseContext()).resetDB();
+//        new TrackDbHelper(getBaseContext()).resetDB();
 
         mPagerAdapter = new HomeScreenPageAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.pager);
